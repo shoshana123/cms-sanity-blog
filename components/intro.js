28cefe54232403b304c1e10a856672f0acab9fc7
Shoshana Rosenfield <shoshana.rosenfield@mac-ldn-ymd6r.home>
2020-08-03 10:56:47 +0100

    update title

--- components/intro.js
+++ components/intro.js
@@ -3,9 +3,9 @@ import { CMS_NAME, CMS_URL } from '../lib/constants'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+      <h2 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Zego Test Blog
+      </h2>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{' '}
         <a
